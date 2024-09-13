de7ca09105fe4f7ef8bc734a8299f4a4db85a0d6
emmilyeduarda2C <emmilyeduarda21@gmail.com>
2024-09-13 07:57:41 -0300

    1

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-const avanca = document.querySelectorALI('.btn-proximo')
+const avanca = document.querySelectorALI('.btn-proximo');
+
+avanca.forEach(button => {
+ button.addEventListener('click' , function(){
+    const atual = document.querySelector('.ativo');  
+    const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+
+    atual.classList.remove('ativo');
+    document.getElementByid(proximoPasso).classList.add('ativo');
+    
+
+ })
+
+})
